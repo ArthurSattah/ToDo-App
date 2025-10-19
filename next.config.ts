3f0ad75c3ca62b617dd4b7d9ddefe68c4fa7a890
Arthur Sattah <arthursattah@gmail.com>
2025-10-19 21:26:57 +0300

    edit the next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // <-- this enables static export
+  reactStrictMode: true, // optional but recommended
 };
 
 export default nextConfig;
